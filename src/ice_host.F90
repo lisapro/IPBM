1b225edbe8ca210bdc681afac84a38a22c06f4f2
shamil <shamilyakubov@hotmail.com>
2016-09-18 09:11:23 +0300

    Remake ice-host 1

--- src/ice_host.F90
+++ src/ice_host.F90
@@ -1,89 +1,124 @@
 module ice_host
   use types
+  use list_mod
+  use input
+  use fabm_driver
 
   implicit none
   private
   public type_host_model
 
-  type type_host_model
-    type(alone_variable),allocatable:: dt
-    type(alone_variable),allocatable:: wind_speed
-    type(alone_variable),allocatable:: mole_fraction_of_carbon_dioxide_in_air
-    type(alone_variable),allocatable:: year
-    type(alone_variable),allocatable:: day
-    type(alone_variable),allocatable:: number_of_layers
-    type(alone_variable),allocatable:: number_of_ice_layers
-    type(alone_variable),allocatable:: number_of_state_variables
-
-    type(variable_1d),allocatable:: ice_thickness
-    type(variable_1d),allocatable:: snow_thickness
-    type(variable_1d),allocatable:: ice_temperature
-
-    type(variable_1d),allocatable:: z
-    type(variable_1d),allocatable:: z_boundary
-    type(variable_1d),allocatable:: time
-    type(variable_1d),allocatable:: dz
-    type(variable_1d),allocatable:: air_pressure
-    type(variable_1d),allocatable:: downwelling_photosynthetic_radiative_flux
-    type(variable_1d),allocatable:: kz_bio
-
-    type(variable_2d),allocatable:: temperature
-    type(variable_2d),allocatable:: practical_salinity
-    type(variable_2d),allocatable:: density_anomaly
-    type(variable_2d),allocatable:: kz
-
-    type(state_variable),allocatable,dimension(:):: state_variables
+  type,extends(list):: type_host_model
   contains
     private
     procedure:: initialize
+    procedure:: add_var
+    procedure:: get_one_var
+    procedure:: get_alone_variable
+    procedure:: get_variable_1d
+    procedure:: get_variable_2d
+    generic:: get_var => get_alone_variable,&
+      get_variable_1d, get_variable_2d
   end type
 
   interface type_host_model
-    module procedure model_constructor
+    module procedure type_host_constructor
   end interface
 contains
-  function model_constructor()
-    type(type_host_model):: model_constructor
+  function type_host_constructor()
+    type(type_host_model):: type_host_constructor
 
-    call model_constructor%initialize()
+    call type_host_constructor%initialize()
   end function
 
   subroutine initialize(self)
-    use input
-
     class(type_host_model):: self
-    type(type_input):: kara_input
-    class(*),allocatable:: foo
 
     kara_input = type_input('KaraSea.nc')
 
     !vertical variables
-    call kara_input%get_input('depth',self%z)
-    call self%z%inverse()
-    call kara_input%get_input('depth_w',self%z_boundary)
-    call self%z_boundary%inverse()
+    call self%add_var('depth')
+    call self%add_var('depth_w')
     !horizontal variables
-    call kara_input%get_input('ocean_time',self%time)
-    call kara_input%get_input('Pair',self%air_pressure)
-    call kara_input%get_input(&
-    'shflux',self%downwelling_photosynthetic_radiative_flux)
+    call self%add_var('ocean_time')
+    call self%add_var('Pair')
+    call self%add_var('shflux')
     !2d variables
-    call kara_input%get_input('temp',self%temperature)
-    call self%temperature%inverse()
-    call kara_input%get_input('salt',self%practical_salinity)
-    call self%practical_salinity%inverse()
-    call kara_input%get_input('rho',self%density_anomaly)
-    call self%density_anomaly%inverse()
-    call kara_input%get_input('AKv',self%kz)
-    call self%kz%inverse()
+    call self%add_var('temp')
+    call self%add_var('salt')
+    call self%add_var('rho')
+    call self%add_var('AKv')
+  end subroutine
 
-    !weird thing!
-    allocate(foo, source=self%z)
-    select type(foo)
+  subroutine add_var(self,inname)
+    class(type_host_model)   :: self
+    character(len=*),intent(in):: inname
+    class(variable),allocatable:: var
+    class(*),allocatable:: temp
+
+    call kara_input%get_input(inname,var)
+    allocate(temp,source=var)
+    call self%add_item(temp)
+  end subroutine
+
+  function get_one_var(self,inname)
+    class(type_host_model):: self
+    character(len=*),intent(in):: inname
+    class(*),pointer:: get_one_item
+
+    call self%reset()
+    do while(self%moreitems())
+      get_one_item => self%get_item()
+      select type(get_one_item)
+      class is(variable)
+        if (trim(get_one_item%name)==trim(inname)) return
+      end select
+      call self%next()
+    end do
+    call fatal_error("Getting variables from NetCDF file",&
+                     "can't find '"//inname//&
+                     "' variable in the NetCDF file")
+  end function
+
+  subroutine get_alone_variable(self,inname,get_alone_var)
+    class(type_host_model):: self
+    character(len=*),intent(in):: inname
+    type(alone_variable),allocatable,intent(out):: get_alone_var
+    class(*),pointer:: curr
+
+    curr => self%get_one_item(inname)
+    select type(curr)
+    type is(alone_variable)
+      allocate(get_alone_var,source=curr)
+      return
+    end select
+  end subroutine
+
+  subroutine get_variable_1d(self,inname,get_var_1d)
+    class(type_host_model):: self
+    character(len=*),intent(in):: inname
+    type(variable_1d),allocatable,intent(out):: get_var_1d
+    class(*),pointer:: curr
+
+    curr => self%get_one_item(inname)
+    select type(curr)
     type is(variable_1d)
-      call foo%inverse()
+      allocate(get_var_1d,source=curr)
+      return
+    end select
+  end subroutine
+
+  subroutine get_variable_2d(self,inname,get_var_2d)
+    class(type_host_model):: self
+    character(len=*),intent(in):: inname
+    type(variable_2d),allocatable,intent(out):: get_var_2d
+    class(*),pointer:: curr
+
+    curr => self%get_one_item(inname)
+    select type(curr)
     type is(variable_2d)
-      call foo%inverse()
+      allocate(get_var_2d,source=curr)
+      return
     end select
   end subroutine
 end module
