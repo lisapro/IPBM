ee8658af130d6c722ee0b852c91cf21b32d8cfaa
shamil <shamilyakubov@hotmail.com>
2016-12-26 21:38:42 +0300

    At work

---
+++ build_release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p build
+rm -r build/*
+CWD=`pwd`
+cp data/* build &&
+cd build && cmake $CWD/src -DFABM_BASE=$FABMDIR \
+-DFABM_NIVA_BASE=$CWD/../brom_niva_module -DCMAKE_BUILD_TYPE=Release
