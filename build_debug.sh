b5c648459ec1a287bc79e18d776d3a8636ba769e
shamil <shamilyakubov@hotmail.com>
2017-08-31 09:21:56 +0200

    Fix shell scripts

--- build_debug.sh
+++ build_debug.sh
@@ -5,6 +5,6 @@ rm -r build/*
 CWD=`pwd`
 cp data/* build &&
 cd build && cmake $CWD/src -DFABM_BASE=$FABMDIR \
-                           -DFABM_ERSEM_BASE=$CWD/../ersem \
+                           -DFABM_ERSEM_BASE=$CWD/../ERSEM \
 -DFABM_NIVA_BASE=$CWD/../brom_niva_module -DCMAKE_BUILD_TYPE=Debug
                            #-DERSEM_USE_IRON=ON \
